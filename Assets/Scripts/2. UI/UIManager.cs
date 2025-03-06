ee6439ad086480d15d641a8b5a7d9fc922e525fb
S-SoHyun <slj506@naver.com>
2025-03-06 18:07:25 +0900

    [Refact] 제네릭 싱글톤 추가
    + 스크립트들 폴더에 나눠서 정리

--- Assets/Scripts/2. UI/UIManager.cs
+++ Assets/Scripts/2. UI/UIManager.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
