64bdba45b3eae747229f9d4742141939a1c575b1
S-SoHyun <slj506@naver.com>
2025-03-08 00:40:33 +0900

    [Feat] 바다에 닿을 경우 데미지 입도록 구현
    not requirement

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
     private static T _instance;
     public static T Instance
