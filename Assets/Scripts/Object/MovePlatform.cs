ee6f16aa17597baf6f54d9b75b8e6243649fd747
S-SoHyun <slj506@naver.com>
2025-03-10 19:22:40 +0900

    [Feat] 움직이는 플랫폼 구현

---
+++ Assets/Scripts/Object/MovePlatform.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    public float maxDistance;
+    private bool isForward;
+    private bool isBackward;
+
+    private Player player;
+
+    private void Awake()
+    {
+        player = CharacterManager.Instance.Player;
+    }
+
+    void Start()
+    {
+        ForwardMove();
+    }
+
+    void Update()
+    {
+        if (transform.position == targetPosition && isBackward)
+        {
+            ForwardMove();
+        }
+        else if (transform.position == targetPosition && isForward)
+        {
+            BackwardMove();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, 1f * Time.deltaTime);
+    }
+
+    void ForwardMove()
+    {
+        targetPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + maxDistance);
+        isForward = true;
+        isBackward = false;
+    }
+
+    void BackwardMove()
+    {
+        targetPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - maxDistance);
+        isForward = false;
+        isBackward = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = this.gameObject.transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
